76f4ac64d313b1482533f530a2385eb4f1398dd0
Mariia Pinchak <marypinchak1@gmail.com>
2023-05-19 17:40:12 +0300

    Update index.tsx

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -30,7 +30,9 @@ const Slider = () => {
                     onSwiper={(swiper) => console.log(swiper)}
                     onSlideChange={() => console.log('slide change')}
                 >
-                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>
+                Slide 1
+                    </SwiperSlide>
                     <SwiperSlide>Slide 2</SwiperSlide>
                     <SwiperSlide>Slide 3</SwiperSlide>
                     <SwiperSlide>Slide 4</SwiperSlide>
